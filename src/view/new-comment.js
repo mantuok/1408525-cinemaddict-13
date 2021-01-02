0a5d3d70203a48b87a2f3dd98b8391d0f25e6f26
mantuok <mantuok@gmail.com>
2021-01-03 01:50:02 +0300

    Исправления по комментариям, m6t1 WIP

--- src/view/new-comment.js
+++ src/view/new-comment.js
@@ -1,4 +1,4 @@
-import AbstractView from "./abstract.js";
+import SmartView from "./smart.js";
 
 const Emoji = {
   SMILE: `smile`,
@@ -14,11 +14,19 @@ const createEmojiItemTemplate = (emoji) => {
     </label>`;
 };
 
-const createNewCommentTemplate = () => {
+const createNewCommentTemplate = (message) => {
+  const {emotion, text} = message;
+
+  const setEmotionTemplate = () =>
+    emotion !== null ? `<img src="images/emoji/${emotion}.png" width="55" height="55" alt="emoji-${emotion}">`
+    : ``;
+
   return `<div class="film-details__new-comment">
-  <div class="film-details__add-emoji-label"></div>
+  <div class="film-details__add-emoji-label">
+    ${setEmotionTemplate()}
+  </div>
   <label class="film-details__comment-label">
-    <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
+    <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment">${text}</textarea>
   </label>
   <div class="film-details__emoji-list">
   ${createEmojiItemTemplate(Emoji.SMILE)}
@@ -29,8 +37,62 @@ const createNewCommentTemplate = () => {
 </div>`;
 };
 
-export default class NewComment extends AbstractView {
+export default class NewComment extends SmartView {
+  constructor() {
+    super();
+
+    this._data = {
+      emotion: null,
+      text: ``,
+      scroll: 0
+    }
+
+    this._emojiClickHandler = this._emojiClickHandler.bind(this);
+    this._commentInputHandler = this._commentInputHandler.bind(this);
+    this._setScrollValue = this._setScrollValue.bind(this);
+
+    this._setInnerHandlers();
+  }
+
   getTemplate() {
-    return createNewCommentTemplate();
+    return createNewCommentTemplate(this._data);
+  }
+
+  restoreHandlers() {
+    this._setInnerHandlers();
+  }
+
+  restoreState() {
+    this._setScrollValue();
+  }
+
+  _emojiClickHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      emotion: evt.target.value
+    }, false)
+  };
+
+  _commentInputHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      text: evt.target.value,
+      scroll: evt.target.scrollTop
+    }, true)
+  }
+
+  _setInnerHandlers() {
+    this.getElement()
+        .querySelectorAll(`.film-details__emoji-item`)
+        .forEach(element => element.addEventListener(`click`, this._emojiClickHandler));
+    this.getElement()
+        .querySelector(`.film-details__comment-input`)
+        .addEventListener(`input`, this._commentInputHandler)
+  }
+
+  _setScrollValue() {
+    this.getElement()
+        .querySelector(`.film-details__comment-input`)
+        .scrollTop = this._data.scroll;
   }
 }
